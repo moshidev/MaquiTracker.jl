76ed8c57dbac9e9b5ad9c55b66403cda6a08e5b2
josemponce <josemapo3@gmail.com>
2023-10-26 10:59:19 +0200

    Añadido struct EstructuraDatosTiempoReal
    Estructura para representar los datos que se reciben de los sensores, closes #16

---
+++ src/datos_tiempo_real.jl
@@ -0,0 +1,14 @@
+
+# Struct EstructuraDatosTiempoReal
+# Objeto Valor donde guardamos los reportes de los sensores
+
+mutable struct EstructuraDatosTiempoReal
+
+    usos::Int64
+    tiempoFuncionamiento::Float32
+    aceite::Float32
+    presion::Float32
+    electroValvulas::Float32
+    antiguedad::Float32
+
+end
