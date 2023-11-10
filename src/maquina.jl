a74c9876101e1bd8ac0c9c0abf3c64c847943e5c
josemponce <josemapo3@gmail.com>
2023-11-10 13:19:05 +0100

    Modificado struct Maquina
    Variables para poder detectar fallos closes #24

--- src/maquina.jl
+++ src/maquina.jl
@@ -1,6 +1,3 @@
-
-using Interpolations
-
 # Struct Maquina
 # Entidad
 
@@ -9,5 +6,11 @@ mutable struct Maquina
     # Variables para representar los fallos que se detecten
 
     problema::String
+
+    # Operaciones necesarias para poder realizar la deteccion de problemas de la máquina
+
+    mediaTemperatura::Float32
+    mediaPresion::Float32
+    mediaEV::Float32
 
 end
