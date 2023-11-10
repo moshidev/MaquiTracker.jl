a9bb1e65899aee18a52fe7e97f95bb50da13a48d
josemponce <josemapo3@gmail.com>
2023-11-10 14:44:24 +0100

    Modificado struct Maquina closes #25 #26
    Modificado mutabilidad variables

--- src/maquina.jl
+++ src/maquina.jl
@@ -1,23 +1,20 @@
 # Struct Maquina
 # Entidad
 
+include("medias.jl")
+
 mutable struct Maquina
 
     # Variables para representar los fallos que se detecten
 
     problema::String
-
-    # Operaciones necesarias para poder realizar la deteccion de problemas de la máquina
-
-    mediaTemperatura::Float32
-    mediaPresion::Float32
-    mediaEV::Float32
-
-    # Variables para definir los intervalos de funcionamiento correcto
-
-    maxPresion::Int32
-    minTemAceite::Int32
-    temAceiteAlta::Int32
-    temAceitePeligrosa::Int32
+    medias::Medias
 
 end
+
+# Variables para definir los intervalos de funcionamiento correcto
+
+const maxPresion = 180
+const minTemAceite = 40
+const temAceiteAlta = 70
+const temAceitePeligrosa = 90
---
+++ src/medias.jl
@@ -0,0 +1,12 @@
+# Struct Medias
+# Objeto Valor
+
+struct Medias
+
+    # Variables necesarias para poder realizar la deteccion de problemas de la máquina
+
+    mediaTemperatura::Float32
+    mediaPresion::Float32
+    mediaEV::Float32
+
+end
