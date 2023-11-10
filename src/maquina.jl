309dc67643089552bb2e9bffa5af637028ebd5e9
josemponce <josemapo3@gmail.com>
2023-11-10 14:08:34 +0100

    Modificado struct Maquina
    Variables para intervalo de buen funcionamiento closes #25 #26

--- src/maquina.jl
+++ src/maquina.jl
@@ -13,4 +13,11 @@ mutable struct Maquina
     mediaPresion::Float32
     mediaEV::Float32
 
+    # Variables para definir los intervalos de funcionamiento correcto
+
+    maxPresion::Int32
+    minTemAceite::Int32
+    temAceiteAlta::Int32
+    temAceitePeligrosa::Int32
+
 end
