35468ff73a3ecc8c2984d635345d250a1bd88de6
charfield <46326833+moshidev@users.noreply.github.com>
2024-01-27 17:53:20 +0000

    da formato uniforme al código (#49)

--- src/maquitracker.jl
+++ src/maquitracker.jl
@@ -1,71 +1,71 @@
 module maquitracker
-    using Statistics
-    export EV_es_defectuosa
+using Statistics
+export EV_es_defectuosa
 
-    struct Medias
-        # Variables necesarias para poder realizar la deteccion de problemas de la máquina
-        mediaTemperatura::Float32
-        mediaPresion::Float32
-        mediaEV::Float32
+struct Medias
+    # Variables necesarias para poder realizar la deteccion de problemas de la máquina
+    mediaTemperatura::Float32
+    mediaPresion::Float32
+    mediaEV::Float32
+end
+
+# Struct Maquina
+# Entidad
+mutable struct Maquina
+
+    # Variables para representar los fallos que se detecten
+
+    problema::String
+    medias::Medias
+
+end
+
+# Variables para definir los intervalos de funcionamiento correcto
+const maxPresion = 180
+const minTemAceite = 40
+const temAceiteAlta = 70
+const temAceitePeligrosa = 90
+const minEV = 1.0   # minimo valor de corriente de EV en amperios
+const maxEV = 4.0   # maximo valor de corriente de EV en amperios
+
+"""
+EV_es_defectuosa(c::Vector{Float64}, t::Vector{Float64})
+
+Esta función verifica si un electroválvula (EV) es defectuosa. Para ello, realiza las siguientes comprobaciones:
+
+- Verifica que los vectores `c` y `t` tengan la misma longitud.
+- Verifica que los vectores `c` y `t` no estén vacíos.
+- Verifica que todos los elementos de `c` estén dentro del rango [minEV, maxEV].
+- Calcula el factor de `c` y `t`, y luego calcula la pendiente promedio de este factor.
+- Calcula el cambio relativo de la pendiente en porcentaje.
+- Si el cambio relativo es mayor o igual al 0.05%, la función devuelve `true`, indicando que la EV es defectuosa. De lo contrario, devuelve `false`.
+
+# Argumentos
+- `c::Vector{Float64}`: Un vector que representa la corriente de la EV en amperios.
+- `t::Vector{Float64}`: Un vector que representa la temperatura de la EV en grados Celsius.
+
+# Devoluciones
+- `Bool`: `true` si la EV es defectuosa, `false` en caso contrario.
+"""
+function EV_es_defectuosa(c::Vector{Float64}, t::Vector{Float64})
+    if length(c) != length(t)
+        throw(DimensionMismatch("Los vectores c y t deben tener la misma longitud"))
     end
 
-    # Struct Maquina
-    # Entidad
-    mutable struct Maquina
-
-        # Variables para representar los fallos que se detecten
-
-        problema::String
-        medias::Medias
-
+    if length(c) == 0 || length(t) == 0
+        throw(ArgumentError("Los vectores c y t no deben tener longitud 0"))
     end
 
-    # Variables para definir los intervalos de funcionamiento correcto
-    const maxPresion = 180
-    const minTemAceite = 40
-    const temAceiteAlta = 70
-    const temAceitePeligrosa = 90
-    const minEV = 1.0   # minimo valor de corriente de EV en amperios
-    const maxEV = 4.0   # maximo valor de corriente de EV en amperios
-
-    """
-    EV_es_defectuosa(c::Vector{Float64}, t::Vector{Float64})
-
-    Esta función verifica si un electroválvula (EV) es defectuosa. Para ello, realiza las siguientes comprobaciones:
-
-    - Verifica que los vectores `c` y `t` tengan la misma longitud.
-    - Verifica que los vectores `c` y `t` no estén vacíos.
-    - Verifica que todos los elementos de `c` estén dentro del rango [minEV, maxEV].
-    - Calcula el factor de `c` y `t`, y luego calcula la pendiente promedio de este factor.
-    - Calcula el cambio relativo de la pendiente en porcentaje.
-    - Si el cambio relativo es mayor o igual al 0.05%, la función devuelve `true`, indicando que la EV es defectuosa. De lo contrario, devuelve `false`.
-
-    # Argumentos
-    - `c::Vector{Float64}`: Un vector que representa la corriente de la EV en amperios.
-    - `t::Vector{Float64}`: Un vector que representa la temperatura de la EV en grados Celsius.
-
-    # Devoluciones
-    - `Bool`: `true` si la EV es defectuosa, `false` en caso contrario.
-    """
-    function EV_es_defectuosa(c::Vector{Float64}, t::Vector{Float64})
-        if length(c) != length(t)
-            throw(DimensionMismatch("Los vectores c y t deben tener la misma longitud"))
-        end
-
-        if length(c) == 0 || length(t) == 0
-            throw(ArgumentError("Los vectores c y t no deben tener longitud 0"))
-        end
-
-        if any(x -> x < minEV || x > maxEV, c)
-            return true
-        end
-        
-        factor = c ./ t
-        x = Vector{Float64}(1:length(factor))
-        dfactor = diff(factor)
-        dx = diff(x)
-        pendiente = mean(dfactor ./ dx)
-        relativo = (pendiente / mean(factor)) * 100
-        return relativo >= 0.05
+    if any(x -> x < minEV || x > maxEV, c)
+        return true
     end
+
+    factor = c ./ t
+    x = Vector{Float64}(1:length(factor))
+    dfactor = diff(factor)
+    dx = diff(x)
+    pendiente = mean(dfactor ./ dx)
+    relativo = (pendiente / mean(factor)) * 100
+    return relativo >= 0.05
+end
 end
