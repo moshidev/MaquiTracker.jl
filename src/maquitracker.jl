45e3194083b255b0da757e899457d3bc8374a550
charfield <46326833+moshidev@users.noreply.github.com>
2024-01-28 20:05:38 +0000

    elimina código muerto (#50)

--- src/maquitracker.jl
+++ src/maquitracker.jl
@@ -2,24 +2,6 @@ module maquitracker
 using Statistics
 export EV_es_defectuosa
 
-struct Medias
-    # Variables necesarias para poder realizar la deteccion de problemas de la máquina
-    mediaTemperatura::Float32
-    mediaPresion::Float32
-    mediaEV::Float32
-end
-
-# Struct Maquina
-# Entidad
-mutable struct Maquina
-
-    # Variables para representar los fallos que se detecten
-
-    problema::String
-    medias::Medias
-
-end
-
 # Variables para definir los intervalos de funcionamiento correcto
 const maxPresion = 180
 const minTemAceite = 40
